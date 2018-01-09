a3463f9cdb6d1749a99c348f922ac6da17846f27
Luojiaxing1991 <luojiaxing@huawei.com>
2018-01-09 19:16:27 +0800

    new yaml generate py 1

--- ci_testcase/sas_autotest/Yaml_generate.py
+++ ci_testcase/sas_autotest/Yaml_generate.py
@@ -2,20 +2,36 @@
 
 import yaml
 
+#the Yaml_conf save the CI config  and static config
 import Yaml_conf as YConf
 
 class CI_yaml(yaml.YAMLObject):
     yaml_tag = u'!CI_yaml'
-    def __init__(self,metadata):
+    def __init__(self,metadata,run,parse):
         self.metadata = metadata
+        self.run      = run
+        self.parse    = parse
     def __repr__(self):
-        return "%s(metadata=%r)"%(self.__class__.__name__,self.metadata)
+        return "%s(metadata=%r,run=%r,parse=$r)"%(self.__class__.__name__,self.metadata,self.run,self.parse)
 
 tmpCfg = YConf.getYConf()
 
-dir_meta = {'os':tmpCfg.ci_os,'maintainer':tmpCfg.nor_maintainer}
+dir_meta = {
+        'name': 'sas_test',
+        'format': tmpCfg.ci_format,
+        'description':'nothing to say',
+        'maintainer':[tmpCfg.nor_maintainer],
+        'os':[tmpCfg.ci_os],
+        'device':[tmpCfg.ci_device],
+        'scope':[tmpCfg.nor_scope],
+        'environment':[tmpCfg.nor_env]
+        }
 
-ci = CI_yaml(dir_meta)
+dir_run = {'step':['sudo ./ci_testcase/sas_autotest/sas_main.sh']}
+
+dir_parse = {'pattern':"(?P<test_case_id>[ /a-zA-Z0-9]+): (?P<result>[A-Z]+)"}
+
+ci = CI_yaml(dir_meta,dir_run,dir_parse)
 
 f = open('test.yaml',"w")
 
