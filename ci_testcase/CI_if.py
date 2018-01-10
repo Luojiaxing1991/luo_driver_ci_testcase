236f3106350625f091087dc64ffea9c2674d327f
Luojiaxing1991 <luojiaxing@huawei.com>
2018-01-10 16:48:18 +0800

    start to do batch processing of ci testcase

---
+++ ci_testcase/CI_if.py
@@ -0,0 +1,46 @@
+#! /usr/bin/env python
+
+import os
+
+import sys
+
+gpus = sys.argv[1]
+batch_size = sys.argv[2]
+
+print gpus
+print batch_size
+
+#This function get the dir name in curr dir
+#return: the name list of dir name
+def file_name(file_dir):
+    for root,dirs,files in os.walk(file_dir):
+        #we need to get the dirlist in current document
+        if root==file_dir:
+            return dirs
+
+    return none
+
+#get the current path
+pwd = os.getcwd()
+
+#get the dir name list
+dirlist = file_name(pwd)
+
+#output the dir name for debug
+print(dirlist)
+
+#new a list used to save the ci dir among the dir name before
+cidir = []
+
+#we find the dir according to keyword,
+#the key word used to place in last of dirname
+keyword = 'autotest'
+
+for i in range(len(dirlist)):
+    if dirlist[i][-len(keyword):] == keyword:
+        cidir.append(dirlist[i])
+
+#output the ci dir name
+print(cidir)
+
+
