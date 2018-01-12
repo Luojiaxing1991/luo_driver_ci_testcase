e1c3446c3684f3d21e71a1d5dec28cc19d040929
Luojiaxing1991 <luojiaxing@huawei.com>
2018-01-12 16:21:13 +0800

    f

---
+++ ci_testcase/sas_autotest/AddPath.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+
+import os
+
+pwd = os.getcwd()
+
+tmpPath = [pwd]
+
+tmp1 = pwd.split("/")
+
+tmp1.pop(0)
+
+tmpLen = 0
+
+for name in tmp1:
+    if name == 'ci_testcase':
+        break
+    tmpLen = tmpLen + len(name) + 1
+
+targetdir='%s/bin'%pwd[0:tmpLen]
+
+os.system('PATH="${targetdir}:PATH"')
+
