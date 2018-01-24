69ba4906a6c7fec14f3fc67025f976cda9b88181
Luojiaxing1991 <luojiaxing@huawei.com>
2018-01-24 13:40:30 +0800

    ,

--- ci_testcase/roce_autotest/roce_main.sh
+++ ci_testcase/roce_autotest/roce_main.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-# Load common function
-. roce_autotest/config/roce_test_lib
+CUR=$(cd "`dirname $0`" ; pwd)
 
+echo ${CUR}
+
+# Load common function
+#. roce_autotest/config/roce_test_lib
+. ${CUR}/config/roce_test_lib
 
 # Main operation function
 # IN : N/A
@@ -30,12 +34,34 @@ function main()
     done
 }
 
-# Output log file header
-writeLogHeader
+#get the internet ip of servcie
+LC_ALL=C ifconfig | grep 'inet addr:' | grep -v '127.0.0.1' | cut -d: -f2 | awk '{print $1}'
 
-TrustRelation ${BACK_IP}
-copy_tool_so
-main
+CurrIp=$1
+print ${CurrIp}
+#get the BACK_IP
+declare -A ip_map
+ip_map=(
+["192.168.3.238"]="192.168.3.211"
+["192.168.3.211"]="192.168.3.238"
+)
+
+for tmpip in "${!ip_map[@]}"
+do
+ if ${tmpip} == $CurrIp
+ then
+	BACK_IP=${ip_map[$tmpip]}
+ fi
+done
+
+
+
+# Output log file header
+#writeLogHeader
+
+#TrustRelation ${BACK_IP}
+#copy_tool_so
+#main
 
 echo "testmain finish"
 # clean exit so lava-test can trust the results
